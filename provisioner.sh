653d0a0750fd2a144b61f9d1b32e4437e4484cf2
Emory Merryman <emory.merryman@deciphernow.com>
2016-12-31 09:58:33 -0500

    fix ampersand error

--- provisioner.sh
+++ provisioner.sh
@@ -4,7 +4,7 @@ dnf update --assumeyes &&
   /bin/sh /vagrant/install-virtual-box-extensions.sh &&
   /bin/sh /vagrant/install-docker.sh &&
   /bin/sh /vagrant/install-workstation.sh &&
-  dnf remove --assumeyes gnome-initial-setup 
+  dnf remove --assumeyes gnome-initial-setup &&
   dnf update --assumeyes &&
   dnf clean all &&
   true
