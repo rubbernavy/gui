68b89396e7ff4fe94de87b353dbf817e7302d79b
Emory Merryman <emory.merryman@deciphernow.com>
2016-12-31 10:23:39 -0500

    fixed indenting

--- box.sh
+++ box.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
 COMMIT_ID=$(git rev-parse --verify HEAD) &&
-WORK_DIR=$(mktemp -d) &&
-vagrant destroy -f &&
-vagrant provision &&
-vagrant up &&
-sleep 30s &&
-vagrant halt &&
-vagrant up &&
-sleep 30s &&
-vagrant halt &&
-vagrant up &&
-sleep 30s &&
-vagrant halt &&
-vagrant package --output ${WORK_DIR}/gui.${COMMIT_ID}.box &&
-vagrant box add --name gui.${COMMIT_ID} ${WORK_DIR}/gui.${COMMIT_ID}.box &&
-vagrant halt &&
-rm -rf ${WORK_DIR} &&
-true
+  WORK_DIR=$(mktemp -d) &&
+  vagrant destroy -f &&
+  vagrant provision &&
+  vagrant up &&
+  sleep 30s &&
+  vagrant halt &&
+  vagrant up &&
+  sleep 30s &&
+  vagrant halt &&
+  vagrant up &&
+  sleep 30s &&
+  vagrant halt &&
+  vagrant package --output ${WORK_DIR}/gui.${COMMIT_ID}.box &&
+  vagrant box add --name gui.${COMMIT_ID} ${WORK_DIR}/gui.${COMMIT_ID}.box &&
+  vagrant halt &&
+  rm -rf ${WORK_DIR} &&
+  true
